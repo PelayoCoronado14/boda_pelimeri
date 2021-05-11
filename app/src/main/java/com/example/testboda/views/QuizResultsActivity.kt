afc95afb1f2575b8b3208596b3cfb9c608877f5a
Pelayo Coronado <coronado@fidor.com>
2021-05-11 13:30:40 +0200

    Upload results activity and fonts

---
+++ app/src/main/java/com/example/testboda/views/QuizResultsActivity.kt
@@ -0,0 +1,26 @@
+package com.example.testboda.views
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.testboda.R
+
+class QuizResultsActivity :AppCompatActivity(){
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quiz_results)
+        getExtras()
+
+
+
+    }
+
+
+    private fun getExtras(){
+
+
+
+    }
+
+}
